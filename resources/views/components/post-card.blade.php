9f37a4388cfcc88e8e09e0c571a52c9b1b8d95c3
Asem Ahmed <softasem@gmail.com>
2022-08-17 08:43:34 +0300

    add filtering .

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,6 +1,6 @@
 @props(['post'])
 <article
-   {{$attributes->merge(['class'=> 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl'])}}>
+    {{$attributes->merge(['class'=> 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl'])}}>
     <div class="py-6 px-5">
         <div>
             <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
@@ -9,8 +9,7 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <x-category-button :category="$post->category" />
-
+                    <x-category-button :category="$post->category"/>
 
 
                 </div>
@@ -29,9 +28,7 @@
             <div class="text-sm mt-4 space-y-4">
 
 
-                    {!! $post->excerpt !!}
-
-
+                {!! $post->excerpt !!}
 
 
             </div>
@@ -40,8 +37,9 @@
                 <div class="flex items-center text-sm">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
-                        <h5 class="font-bold">{{$post->author->name}}</h5>
-                        <h6>Mascot at Laracasts</h6>
+                        <h5 class="font-bold">
+                            <a href="/?author={{$post->author->username}}">{{$post->author->name}}</a>
+                        </h5>
                     </div>
                 </div>
 
